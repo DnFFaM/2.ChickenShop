3fefaba209fa8f16e18338d8dff324ae319936c3
DnFFaM <projectsmaker1803@outlook.com>
2022-10-07 12:04:56 +0200

    Sql first update

--- Projects I Made/C#/C# Console App(NET F)/sqltest/sqltest/Program.cs
+++ Projects I Made/C#/C# Console App(NET F)/sqltest/sqltest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace sqltest
 {
@@ -10,6 +11,37 @@ namespace sqltest
     {
         static void Main(string[] args)
         {
+            string login = "server = 172.19.176.160; uid = JJ; pwd = 1234; database = Test";
+            SqlConnection conn = new SqlConnection();
+
+            try
+            {
+                conn.ConnectionString = login;
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+            conn.Open();
+            Console.WriteLine("Du er nu logget ind på databasen!");
+
+
+            Console.Write("Indtast navn: ");
+            string navn = Console.ReadLine();
+            Console.Write("Indtast alder: ");
+            int alder = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Indtast adresse: ");
+            string adr = Console.ReadLine();
+            // INSERT INTO tabelnavn (kolonne1, kolonne2, kolonne3)
+            SqlCommand cmd = new SqlCommand($"INSERT INTO Person (Navn, Alder, Adresse) VALUES ('{navn}', '{alder}', '{adr}')", conn);
+            cmd.ExecuteNonQuery();
+            /* 
+             * NonQuery: Opdater databasen, indsæt, slet, rediger værdier
+             * Query: Læs enten alt eller specifikke ting fra databasen
+            */
+            Console.ReadKey();
+
+            conn.Close();
         }
     }
 }
